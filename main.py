474c92f926a78987178e5c99d323473a951fad70
I-Rusel-I <veterlvlpole@gmail.com>
2024-08-17 12:33:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Hello world")
+#"1st program"
+print (9**0.5*5)
+# "2st program"
+print (9.99>9.98)and(1000!=1000) #boolean
+#"3st program"
+print ((2*2+2)==2*(2+2)) #boolean
+#"4st program"
+x = float('123.456')
+y = (x*10)
+print (int(y%10))
+
+
